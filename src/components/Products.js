0d2f33f8643c1632029935d2116bec0bee762914
Eddydebuisson <eddydebuisson@gmail.com>
2022-05-13 16:43:55 +0200

    ajout du prix et du panier

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,9 +1,13 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { createPortal } from 'react-dom';
+import { typeImplementation } from '@testing-library/user-event/dist/type/typeImplementation';
 
 function Products() {
   const [posts, setposts] = useState(null);
   const [product,setproduct] = useState("");
+  const [panier,setpanier] = useState(null);
+  const [total,settotal] = useState(0);
 
   useEffect( () => {
     getAllData() ;
@@ -22,9 +26,77 @@ function Products() {
     
   }
 
+function decrement(index) {
+
+  let items = [...posts];
+  let item = {...items[index.target.id]}
+  if(item.quantitySelected > 0){
+  item.quantitySelected = item.quantitySelected - 1; 
+  items[index.target.id] = item;
+   setposts(items);
+  }
+}
+
+function increment(index) {
+  let items = [...posts];
+  let item = {...items[index.target.id]};
+
+  item.quantitySelected = item.quantitySelected + 1;
+  item.addedToBasket = true
+  items[index.target.id] = item;
+   setposts(items);
+  
+}
+
+function addme(){
+  setpanier(posts.filter(p => p.addedToBasket))
+  
+}
+
+
+
+ function reset(id){
+  const remove = panier.filter( i => i.id != id.target.id);
+  setpanier(remove);
+  
+ }
+
+//  function totalBasket(){
+//    let sumPrix = 0;
+ 
+
+//   panier.forEach(element => {
+//     sumPrix += element.prix * element.quantitySelected;
+//   });
+//   console.log(sumPrix)
+//   settotal(sumPrix) ;
+
+//   return total;
+
+//  }
+
+
+
+
   return (
  <>
-    <h1>API Posts</h1>
+    <h1>KIRK ENTERPRISE</h1>
+
+    <div>
+      <ul>
+      {panier && panier.map( p => {
+        return (
+          <>
+          <li key={p.id}>{p.title} : {p.prix*p.quantitySelected} </li>
+          <button id={p.id} onClick={reset.bind(p.id)}>X</button>
+          </>
+          )
+  })}
+  {/* {console.log(panier)} */}
+      </ul>
+      <label>{total}</label>
+    </div>
+
     <input list="produits" placeholder="Enter Post Title" onChange={event => setproduct(event.target.value)} />
     <datalist  id="produits">
     {posts && posts.map( p => {
@@ -34,6 +106,7 @@ function Products() {
           )
   })}
     </datalist>
+  <div className='container'>
   <ul>
   {posts && posts.filter(post => {
     if (product === '') {
@@ -48,12 +121,17 @@ function Products() {
                 <h3>{p.title}</h3>
                 <p>{p.description}</p>
                 <img src={p.image} ></img>
-                <p>{p.description}</p>
                 <p>{p.prix}</p>
+                <button id={p.id} onClick={decrement.bind(p.id)}>-</button>
+                <label>{p.quantitySelected}</label>
+                <button id={p.id} onClick={increment.bind(p.id)}>+</button>
+                <button id={p.id} onClick={addme.bind()}>Add me</button>
+
           </li>
           )
   })}
   </ul> 
+  </div>
  </>
   );
 }
