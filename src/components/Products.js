dd1da299923b5d31afe5ecbd149618e3245d0a6a
nihylin <nihylin@gmail.com>
2022-05-17 00:17:30 +0200

    Ajout pages Boutique + Vitrine

--- src/components/Products.js
+++ src/components/Products.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { createPortal } from 'react-dom';
 import { typeImplementation } from '@testing-library/user-event/dist/type/typeImplementation';
 
+
 function Products() {
   const [posts, setposts] = useState(null);
   const [product,setproduct] = useState("");
@@ -22,8 +23,8 @@ function Products() {
       const p = res.data;
       setposts( p );
     });
-    
-    
+
+
   }
 
 function decrement(index) {
@@ -31,7 +32,7 @@ function decrement(index) {
   let items = [...posts];
   let item = {...items[index.target.id]}
   if(item.quantitySelected > 0){
-  item.quantitySelected = item.quantitySelected - 1; 
+  item.quantitySelected = item.quantitySelected - 1;
   items[index.target.id] = item;
    setposts(items);
   }
@@ -45,12 +46,12 @@ function increment(index) {
   item.addedToBasket = true
   items[index.target.id] = item;
    setposts(items);
-  
+
 }
 
 function addme(){
   setpanier(posts.filter(p => p.addedToBasket))
-  
+
 }
 
 
@@ -58,12 +59,12 @@ function addme(){
  function reset(id){
   const remove = panier.filter( i => i.id != id.target.id);
   setpanier(remove);
-  
+
  }
 
 //  function totalBasket(){
 //    let sumPrix = 0;
- 
+
 
 //   panier.forEach(element => {
 //     sumPrix += element.prix * element.quantitySelected;
@@ -75,37 +76,47 @@ function addme(){
 
 //  }
 
-
-
-
   return (
  <>
-    <h1>KIRK ENTERPRISE</h1>
 
-    <div>
-      <ul>
+ <header>
+   <img src="img/horizontalCouleur.png" alt="PROUT" />
+   <div className='recherche'>
+    <input list="produits" placeholder="Enter Post Title" onChange={event => setproduct(event.target.value)} />
+   </div>
+
+   <datalist  id="produits">
+   {posts && posts.map( p => {
+       return (
+
+         <option key={p.id} value={p.title}>{p.title}</option>
+         )
+ })}
+   </datalist>
+   <nav>
+     <a href="">Boutique</a>
+     <a href="">Contact</a>
+     <a href="">Chépa</a>
+     <a href="">Chépanonplus</a>
+   </nav>
+ </header>
+
+    <div className='basket'>
+      <ul >
+        <h3>Panier</h3>
       {panier && panier.map( p => {
         return (
           <>
-          <li key={p.id}>{p.title} : {p.prix*p.quantitySelected} </li>
-          <button id={p.id} onClick={reset.bind(p.id)}>X</button>
+          <li key={p.id}>{p.title} : {p.prix*p.quantitySelected} €           <button id={p.id} onClick={reset.bind(p.id)}>X</button></li>
           </>
           )
   })}
   {/* {console.log(panier)} */}
       </ul>
-      <label>{total}</label>
+      <label>Total : {total} €</label>
     </div>
 
-    <input list="produits" placeholder="Enter Post Title" onChange={event => setproduct(event.target.value)} />
-    <datalist  id="produits">
-    {posts && posts.map( p => {
-        return (
 
-          <option key={p.id} value={p.title}>{p.title}</option>
-          )
-  })}
-    </datalist>
   <div className='container'>
   <ul>
   {posts && posts.filter(post => {
@@ -121,16 +132,17 @@ function addme(){
                 <h3>{p.title}</h3>
                 <p>{p.description}</p>
                 <img src={p.image} ></img>
-                <p>{p.prix}</p>
-                <button id={p.id} onClick={decrement.bind(p.id)}>-</button>
-                <label>{p.quantitySelected}</label>
-                <button id={p.id} onClick={increment.bind(p.id)}>+</button>
-                <button id={p.id} onClick={addme.bind()}>Add me</button>
-
+                <p>{p.prix} €</p>
+                <div>
+                  <button id={p.id} onClick={decrement.bind(p.id)} className='quantity'>-</button>
+                  <label>{p.quantitySelected}</label>
+                  <button id={p.id} onClick={increment.bind(p.id)} className='quantity'>+</button>
+                  <button id={p.id} onClick={addme.bind()} className='ajouter'>Add me</button>
+                </div>
           </li>
           )
   })}
-  </ul> 
+  </ul>
   </div>
  </>
   );
--- src/index.js
+++ src/index.js
@@ -2,16 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-// import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes,Route } from "react-router-dom";
+import Products from './components/Products';
+import Vitrine from './components/Vitrine';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="produit" element={<Products />} />
+      <Route path="vitrine" element={<Vitrine />} />
+      {/* <Route path="invoices" element={<Invoices />} /> */}
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+reportWebVitals();
